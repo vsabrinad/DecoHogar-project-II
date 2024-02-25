074b9c651e038a26a01d15a764d925cb6baacd7e
Sabrina <sabrinadvillarreal@hotmail.com>
2024-02-24 21:47:31 -0300

    PreEntrega1+Villarreal,Sabrina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {ItemListComponents} from './components/ItemListComponents'
 import './App.css'
 
 function App() {
@@ -9,25 +7,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <a>Inicio</a>
+          <a>¿Quienes somos?</a>
+          <a>Contacto</a>
+          <a>Productos</a>
+
+        </nav>
       </div>
-      <h1>Vite + React</h1>
+      <h1>OLI'S HOUSE  -decoHogar-</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 11)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/ItemListComponents.jsx
@@ -0,0 +1,11 @@
+export const saludar = () => {
+    return ( <> 
+    <div>
+        <h2 style={{padding: "10 px"}}>Bienvenidos a Oli's house</h2>
+        <p>Todo lo que necesitas para tu hogar.</p>
+    </div>
+    
+    </>
+    )
+}
+export default ItemListComponent;
