cc57a490a3bf59d308c9510821930a77f1567116
Sabrina <sabrinadvillarreal@hotmail.com>
2024-03-01 20:14:54 -0300

    arreglando tp1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import {ItemListComponents} from './components/ItemListComponents'
+import ItemListComponents from './components/ItemListComponents'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <nav>
-          <a>Inicio</a>
-          <a>¿Quienes somos?</a>
-          <a>Contacto</a>
-          <a>Productos</a>
-
-        </nav>
-      </div>
-      <h1>OLI'S HOUSE  -decoHogar-</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 11)}>
-          count is {count}
-        </button>
-      </div>
+      <ItemListComponents/>
     </>
   )
 }
--- src/components/ItemListComponents.jsx
+++ src/components/ItemListComponents.jsx
@@ -1,11 +1,15 @@
-export const saludar = () => {
+import './style.css'
+
+const ItemListComponents = () => {
+    const clickbutton = () => {
+        console.log('click')
+    } 
     return ( <> 
     <div>
-        <h2 style={{padding: "10 px"}}>Bienvenidos a Oli's house</h2>
+        <h2>Bienvenidos a Oli's house</h2>
         <p>Todo lo que necesitas para tu hogar.</p>
+        <button onClick={() => clickbutton()}>Ingresar</button>
     </div>
-    
-    </>
-    )
+    </>);
 }
-export default ItemListComponent;
+export default ItemListComponents;
